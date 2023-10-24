5a7ad3d96556f504684b1409118defe3d4e01da8
Sharukhi <sharukhi@sharukhi.xyz>
2023-10-24 22:07:14 +0600

    added firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+
+const firebaseConfig = {
+  apiKey: "apikey",
+  authDomain: "authDomain",
+  projectId: "projectId",
+  storageBucket: "storageBucket",
+  messagingSenderId: "messagingSenderId",
+  appId: "appId",
+  measurementId: "measurementId"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
