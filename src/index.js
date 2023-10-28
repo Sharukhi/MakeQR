97cd0b387f9a3194afbef8c6da2dabf22c804a32
sharukhi <sharukhi@sharukhi.xyz>
2023-10-28 13:32:32 +0600

    added analytics

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import { Analytics } from '@vercel/analytics/react';
 
 import App from "./App";
 
@@ -10,7 +11,9 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>
+  
 );
 
 serviceWorkerRegistration.register();
